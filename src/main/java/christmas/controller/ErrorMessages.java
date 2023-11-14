006434a342a736293dcfa196d0db81d8397efbeb
SJ70 <seojae70@gmail.com>
2023-11-14 17:50:28 +0900

    fix: 출력 형식 수정
    - 입력 예외 메시지 수정
      - 주문 입력 예외 메시지
      - 날짜 입력 예외 메시지
    
    - 타이틀 메시지 수정
      - 증정 메뉴 타이틀

--- src/main/java/christmas/controller/ErrorMessages.java
+++ src/main/java/christmas/controller/ErrorMessages.java
@@ -2,8 +2,8 @@ package christmas.controller;
 
 enum ErrorMessages {
 
-    INVALID_DATE_INPUT("유효하지 않은 날짜입니다."),
-    INVALID_ORDER_INPUT("유효하지 않은 주문입니다.");
+    INVALID_DATE_INPUT("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDER_INPUT("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/christmas/view/outputView/Messages.java
+++ src/main/java/christmas/view/outputView/Messages.java
@@ -6,7 +6,7 @@ enum Messages {
     PREVIEW_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER("\n<주문 메뉴>"),
     ENTIRE_PRICE("\n<할인 전 총주문 금액>"),
-    GIFTS("\n<증정품>"),
+    GIFTS("\n<증정 메뉴>"),
     DISCOUNT_DETAILS("\n<혜택 내역>"),
     TOTAL_DISCOUNT_AMOUNT("\n<총혜택 금액>"),
     PAYMENT_AMOUNT("\n<할인 후 예상 결제 금액>"),
